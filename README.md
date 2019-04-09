b2082876360ee0336f1f1dc693f901a7c7b6c8d1
usertxt <digitaluser.txt@gmail.com>
2019-04-09 17:11:36 -0500

    Update readme - Make it more clear where your bot's token goes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# discordbot
+# usertxt's Discord Bot
 Simple Discord bot that uses CoinGecko's API to fetch cryptocurrency prices
--- txtbot.py
+++ txtbot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import requests
 import json
 
-token = ''
+token = 'YOUR_TOKEN'
 
 bot = commands.Bot(command_prefix = '!')
 
