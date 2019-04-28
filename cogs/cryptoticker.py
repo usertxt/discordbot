ef2262b50b1ec5f1ca355094e2bbe8a1f137f6f4
usertxt <digitaluser.txt@gmail.com>
2019-04-28 02:06:16 -0500

    Fix caps bug in basecurrency command

--- cogs/cryptoticker.py
+++ cogs/cryptoticker.py
@@ -41,8 +41,7 @@ class CryptoTicker(commands.Cog):
     @commands.command(pass_context=True)
     async def basecurrency(self, ctx, newbase):
         print(f'[CryptoTicker Prompted]: {ctx.message.author}: basecurrency {newbase}')
-        newbase = newbase.lower()
-        if newbase in self.supported_currencies:
+        if newbase.lower() in self.supported_currencies:
             try:
                 self.bot.config["CRYPTOTICKER"]["BASE_CURRENCY"] = newbase.lower()
                 with open(self.bot.configfile, 'w') as updatedconfigfile:
