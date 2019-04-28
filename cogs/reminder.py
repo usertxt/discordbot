3a690bf1bd9f43033dfc7465fc8f22ad2c137e8e
usertxt <digitaluser.txt@gmail.com>
2019-04-28 13:56:31 -0500

    WIP: Reminder Plugin

---
+++ cogs/reminder.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+
+class Reminder(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def remindme(self, ctx, msg, time):
+        if 's' in time:
+            time = int(time.replace('s', ''))
+            await ctx.send(f'Reminding you in {time} seconds to {msg}')
+            await ctx.message.add_reaction('\N{HAMMER AND WRENCH}')
+            print(time)
+            await asyncio.sleep(time)
+            print('CHECK')
+            await discord.User.send(ctx.message.author, f'This is a reminder to {msg}')
+            await ctx.message.clear_reactions()
+            await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+
+        if 'm' in time:
+            time = int(time.replace('m', '')) * 60
+            await ctx.send(f'Reminding you in {time / 60} minutes to {msg}')
+            await ctx.message.add_reaction('\N{HAMMER AND WRENCH}')
+            print(time)
+            await asyncio.sleep(time)
+            print('CHECK')
+            await discord.User.send(ctx.message.author, f'This is a reminder to {msg}')
+            await ctx.message.clear_reactions()
+            await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+
+        if 'h' in time:
+            time = int(time.replace('h', '')) * 3600
+            await ctx.send(f'Reminding you in {time / 3600} hours to {msg}')
+            await ctx.message.add_reaction('\N{HAMMER AND WRENCH}')
+            print(time)
+            await asyncio.sleep(time)
+            print('CHECK')
+            await discord.User.send(ctx.message.author, f'This is a reminder to {msg}')
+            await ctx.message.clear_reactions()
+            await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+
+
+def setup(bot):
+    bot.add_cog(Reminder(bot))
