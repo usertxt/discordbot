89afd3248a7ce259b8a0c575b38f00fefdf094b2
usertxt <digitaluser.txt@gmail.com>
2019-05-02 14:30:50 -0500

    Added all functionality to one command

--- cogs/portfolio.py
+++ cogs/portfolio.py
@@ -41,12 +41,58 @@ class Portfolio(commands.Cog):
         self.supported_coins = requests.get('https://api.coingecko.com/api/v3/coins/list').json()
 
     @commands.command(pass_context=True)
+    async def p(self, ctx, action, ticker=None, quantity=None, price=None):
+        if action == 'positions':
+            disc_id = str(ctx.message.author.id)
+            exists = session.query(
+                session.query(User).filter_by(discord_id=disc_id).exists()
+            ).scalar()
+            if exists is True:
+                if ticker is None:
+                    for discord_id, symbol, quantity, price, in session.query(User.discord_id, User.symbol,
+                                                                              User.quantity, User.price).\
+                                                                              filter(User.discord_id == disc_id):
+                        await ctx.send(f'{symbol.upper()} {quantity} {price}')
+                else:
+                    for discord_id, symbol, quantity, price, in session.query(User.discord_id, User.symbol,
+                                                                              User.quantity, User.price).\
+                                                                              filter(User.discord_id == disc_id).\
+                                                                              filter(User.symbol == ticker):
+                        await ctx.send(f'{symbol.upper()} + {quantity} + {price}')
+            else:
+                await ctx.send('You have no positions')
+
+        elif action == 'add':
+            if ticker in str(self.supported_coins):
+                user = User(name=ctx.message.author.name, discord_id=ctx.message.author.id, symbol=ticker.lower(),
+                            quantity=quantity, price=price)
+                print(f'adding {user} to database')
+                session.add(user)
+                session.commit()
+                session.close()
+                await ctx.send(f'Adding {ticker} {quantity} {price} for {ctx.message.author.name}')
+            else:
+                await ctx.send(f'Error: {ticker} is not a supported currency')
+
+        elif action == 'remove':
+            disc_id = str(ctx.message.author.id)
+            session.query(User). \
+                filter(User.name == str(ctx.message.author.name)). \
+                filter(User.discord_id == disc_id). \
+                filter(User.symbol == ticker). \
+                filter(User.quantity == quantity). \
+                filter(User.price == price). \
+                delete()
+            session.commit()
+            session.close()
+            await ctx.send(f'Removing {ticker} {quantity} {price} for {ctx.message.author.name}')
+
+    @commands.command(pass_context=True)
     async def positions(self, ctx):
         disc_id = str(ctx.message.author.id)
         exists = session.query(
             session.query(User).filter_by(discord_id=disc_id).exists()
         ).scalar()
-        print(exists)
         if exists is True:
             for discord_id, symbol, quantity, price, in session.query(User.discord_id, User.symbol, User.quantity,
                                                                       User.price).filter(User.discord_id == disc_id):
