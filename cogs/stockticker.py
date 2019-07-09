b1c21bf7421dbf80c3e82eacbe4ba9ac0175cbaa
usertxt <digitaluser.txt@gmail.com>
2019-07-09 05:28:01 -0500

    Change command name for a better fit

--- cogs/stockticker.py
+++ cogs/stockticker.py
@@ -12,20 +12,21 @@ class StockTicker(commands.Cog):
         self.url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='
 
     @commands.command(pass_context=True)
-    async def ticker(self, ctx, stock_ticker):
+    async def quote(self, ctx, stock_ticker):
         response = requests.get(self.url + stock_ticker + '&apikey=' + self.key).json()
+        price = float(response['Global Quote']['05. price'])
         logging.info(f'[StockTicker Prompted]: {ctx.message.author}: {stock_ticker}')
         try:
             async with ctx.typing():
                 await asyncio.sleep(1)
-                await ctx.send(f"{stock_ticker.upper()}: ${response['Global Quote']['05. price']}")
-                logging.info(f"[StockTicker Returned]: {stock_ticker.upper()}: ${response['Global Quote']['05. price']}")
+                await ctx.send(f"{stock_ticker.upper()}: ${price:,.2f}")
+                logging.info(f"[StockTicker Returned]: {stock_ticker.upper()}: ${price:,.2f}")
                 await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
-        except Exception:
+        except Exception as e:
             async with ctx.typing():
                 await asyncio.sleep(1)
-                await ctx.send(f'Ticker not found')
-                logging.info('[StockTicker Error]: Ticker not found')
+                await ctx.send(f'{e}')
+                logging.info(f'[StockTicker Error]: {e}')
                 await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
 
 
