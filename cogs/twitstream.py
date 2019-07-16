ebc0cb8c78c82f69e201cdb32c44aa948caf20be
usertxt <digitaluser.txt@gmail.com>
2019-07-16 06:03:29 -0500

    Further simplification of tweet sequencing

--- cogs/twitstream.py
+++ cogs/twitstream.py
@@ -29,10 +29,8 @@ class TwitStream(commands.Cog):
                 await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
         else:
             num = int(count) - 1
-
             data = self.t.statuses.user_timeline(screen_name=screen_name, count=count, tweet_mode="extended")
             twit_id = data[num]['id']
-            num_cont = num
 
             async with ctx.typing():
                 await asyncio.sleep(1)
@@ -41,16 +39,13 @@ class TwitStream(commands.Cog):
                 await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
 
             while data[num]['full_text'].endswith('...'):
-                num_cont -= 1
-                twit_id_cont = data[num_cont]['id']
+                num -= 1
+                twit_id = data[num]['id']
 
                 async with ctx.typing():
                     await asyncio.sleep(3)
-                    await ctx.send(f'https://twitter.com/{screen_name}/status/{twit_id_cont}')
-                    logging.info(f'[TwitStream Returned]: https://twitter.com/{screen_name}/status/{twit_id_cont}')
-
-                if data[num_cont]['full_text'].endswith('...') is False:
-                    break
+                    await ctx.send(f'https://twitter.com/{screen_name}/status/{twit_id}')
+                    logging.info(f'[TwitStream Returned]: https://twitter.com/{screen_name}/status/{twit_id}')
 
 
 def setup(bot):
