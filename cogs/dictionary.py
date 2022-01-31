8ec4fea5ccb243162f5d4837134d29bf01b6e95a
usertxt <digitaluser.txt@gmail.com>
2022-01-31 01:02:54 -0600

    Added real dictionary to dict command and added slash commands to Dictionary cog

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -1,50 +1,63 @@
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
 import discord
-import asyncio
 import requests
 import logging
+import json
 
 
 class Dictionary(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.url = 'https://www.urbandictionary.com/define.php?term='
+        self.urban_url = 'https://www.urbandictionary.com/define.php?term='
+        self.dict_url = 'https://www.merriam-webster.com/dictionary/'
         self.api_url = 'http://api.urbandictionary.com/v0/define?term='
 
-    @commands.command(pass_context=True)
-    async def urban(self, ctx):
-        search_term = ctx.message.content[7:].replace(' ', '+')
-        response = self.url + search_term
+    guild_ids = json.load(open("config.json"))["DISCORD"]["GUILD_IDS"]
+
+    async def dict(self, ctx, search_term):
+        search_term = search_term.replace(' ', '+')
+        response = self.dict_url + search_term
         await ctx.send(response)
-        logging.info(f'[Dictionary Prompted]: {ctx.message.author}: {ctx.message.content[7:]}')
+        logging.info(f'[Dictionary Prompted]: {search_term}')
         logging.info(f'[Dictionary Returned]: {response}')
 
-    @commands.command(pass_context=True)
-    async def dict(self, ctx):
-        search_term_url = ctx.message.content[6:].replace(' ', '+')
-        search_term = ctx.message.content[6:]
+    async def urban(self, ctx, search_term):
+        search_term_url = search_term.replace(' ', '+')
         definition = requests.get(self.api_url + search_term_url).json()
         definition = definition['list'][0]['definition']
-        logging.info(f'[Dictionary Prompted]: {ctx.message.author}: {search_term}')
+        logging.info(f'[Dictionary Prompted]: {search_term}')
 
-        async with ctx.typing():
-            await asyncio.sleep(1)
-            try:
-                e = discord.Embed()
-                if len(definition) > 1024:
-                    e.add_field(name=search_term, value=f'{definition[:1021]}...')
-                elif len(definition) < 1024:
-                    e.add_field(name=search_term, value=definition)
-                e.set_thumbnail(url='https://d2gatte9o95jao.cloudfront.net/assets/apple-touch-icon-1734beeaa059fbc5587bddb3001a0963670c6de8767afb6c67d88d856b0c0dad.png')
-                e.add_field(name='\u200b', value=self.url + search_term_url)
+        try:
+            e = discord.Embed()
+            if len(definition) > 1024:
+                e.add_field(name=search_term, value=f'{definition[:1021]}...')
+            elif len(definition) < 1024:
+                e.add_field(name=search_term, value=definition)
+            e.set_thumbnail(url='https://d2gatte9o95jao.cloudfront.net/assets/apple-touch-icon-1734beeaa059fbc5587bddb3001a0963670c6de8767afb6c67d88d856b0c0dad.png')
+            e.add_field(name='\u200b', value=self.urban_url + search_term_url)
 
-                await ctx.send(embed=e)
-                await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
-                logging.info(f'[Dictionary Returned]: Embed response for {self.api_url + search_term_url}')
-            except IndexError:
-                await ctx.send(f'No results for {search_term}')
-                await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
-                logging.info(f'[Dictionary Error]: No results for {search_term}')
+            await ctx.send(embed=e)
+            logging.info(f'[Dictionary Returned]: Embed response for {self.api_url + search_term_url}')
+        except IndexError:
+            await ctx.send(f'No results for {search_term}')
+            logging.info(f'[Dictionary Error]: No results for {search_term}')
+
+    @cog_ext.cog_slash(name="dict", description="Definitions from Merriam-Webster", guild_ids=guild_ids)
+    async def slash_dict(self, ctx: SlashContext, search_term):
+        await self.dict(ctx, search_term)
+
+    @commands.command(name="dict", pass_context=True)
+    async def command_dict(self, ctx, search_term):
+        await self.dict(ctx, search_term)
+
+    @cog_ext.cog_slash(name="urban", description="Definitions from Urban Dictionary", guild_ids=guild_ids)
+    async def slash_urban(self, ctx: SlashContext, search_term):
+        await self.urban(ctx, search_term)
+
+    @commands.command(name="urban", pass_context=True)
+    async def command_urban(self, ctx, search_term):
+        await self.urban(ctx, search_term)
 
 
 def setup(bot):
