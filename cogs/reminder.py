3381d34436ecc860211280d819493029bf223bb7
usertxt <digitaluser.txt@gmail.com>
2022-02-04 02:40:41 -0600

    Add slash command for Reminder cog

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,49 +1,53 @@
-import discord
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
 import asyncio
 import logging
+import json
 
 
 class Reminder(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
-    async def remindme(self, ctx, time):
-        output = ctx.message.content.split()
-        message = ''
-        for word in output[2:]:
-            message += word
-            message += ' '
+    guild_ids = json.load(open("config.json"))["DISCORD"]["GUILD_IDS"]
 
-        logging.info(f'[RemindMe Prompted]: {ctx.message.author}: {time} {message}')
+    async def remindme(self, ctx, duration, message, slash_check=False):
+        logging.info(f'[RemindMe Prompted]: {ctx.author}: {duration} {message}')
 
-        if 's' in time:
-            time = int(time.replace('s', ''))
-            await ctx.send(f'Reminding you in {time} seconds to {message}')
-            await ctx.message.add_reaction('\N{HAMMER AND WRENCH}')
-            await asyncio.sleep(time)
-            await discord.User.send(ctx.message.author, f'This is a reminder to {message}')
-            await ctx.message.clear_reactions()
-            await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+        if 's' in duration:
+            duration = int(duration.replace('s', ''))
+            if slash_check is True:
+                await ctx.send(f'Reminding you in {duration} seconds to {message}', hidden=True)
+            else:
+                await ctx.send(f'Reminding you in {duration} seconds to {message}')
+            await asyncio.sleep(duration)
+            await ctx.author.send(f'This is a reminder to {message}')
+        elif 'm' in duration:
+            duration = int(duration.replace('m', '')) * 60
+            if slash_check is True:
+                await ctx.send(f'Reminding you in {duration / 60} minutes to {message}', hidden=True)
+            else:
+                await ctx.send(f'Reminding you in {duration / 60} minutes to {message}')
+            await asyncio.sleep(duration)
+            await ctx.author.send(f'This is a reminder to {message}')
+        elif 'h' in duration:
+            duration = int(duration.replace('h', '')) * 3600
+            if slash_check is True:
+                await ctx.send(f'Reminding you in {duration / 3600} hours to {message}', hidden=True)
+            else:
+                await ctx.send(f'Reminding you in {duration / 3600} hours to {message}')
+            await asyncio.sleep(duration)
+            await ctx.author.send(f'This is a reminder to {message}')
 
-        if 'm' in time:
-            time = int(time.replace('m', '')) * 60
-            await ctx.send(f'Reminding you in {time / 60} minutes to {message}')
-            await ctx.message.add_reaction('\N{HAMMER AND WRENCH}')
-            await asyncio.sleep(time)
-            await discord.User.send(ctx.message.author, f'This is a reminder to {message}')
-            await ctx.message.clear_reactions()
-            await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+    @cog_ext.cog_slash(name="remindme", description="Set a reminder", guild_ids=guild_ids)
+    async def slash_remindme(self, ctx: SlashContext, duration, message):
+        await ctx.defer(hidden=True)
+        await self.remindme(ctx, duration, message, slash_check=True)
 
-        if 'h' in time:
-            time = int(time.replace('h', '')) * 3600
-            await ctx.send(f'Reminding you in {time / 3600} hours to {message}')
-            await ctx.message.add_reaction('\N{HAMMER AND WRENCH}')
-            await asyncio.sleep(time)
-            await discord.User.send(ctx.message.author, f'This is a reminder to {message}')
-            await ctx.message.clear_reactions()
-            await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+    @commands.command(name="remindme", pass_context=True, brief="Set a reminder", 
+    description="Example: !remindme 10s \"This is an example\" will make the bot wait 10 seconds, then send you a direct message saying \"This is an example\"")
+    async def command_remindme(self, ctx, duration, message):
+        await self.remindme(ctx, duration, message)
 
 
 def setup(bot):
