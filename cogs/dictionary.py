f15d3dcf759ec0eac4f2b72407636a216ca0fc93
usertxt <digitaluser.txt@gmail.com>
2022-02-10 01:54:00 -0600

    Give embed a cleaner look

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -22,26 +22,30 @@ class Dictionary(commands.Cog):
         logging.info(f'[Dictionary Prompted]: {ctx.author} {search_term}')
         logging.info(f'[Dictionary Returned]: {response}')
 
-    async def urban(self, ctx, search_term):
-        search_term_url = search_term.replace(' ', '+')
-        definition = requests.get(self.api_url + search_term_url).json()
-        definition = definition['list'][0]['definition']
-        logging.info(f'[Dictionary Prompted]: {ctx.author} {search_term}')
-
+    async def urban(self, ctx, search_term, slash_check=False):
+        logging.info(f'[Urban Dictionary Prompted]: {ctx.author}: {search_term}')
         try:
-            e = discord.Embed()
+            search_term_url = search_term.replace(' ', '+')
+            definition = requests.get(self.api_url + search_term_url).json()
+            definition = definition['list'][0]['definition']
+            urban_icon = 'https://d2gatte9o95jao.cloudfront.net/assets/apple-touch-icon-1734beeaa059fbc5587bddb3001a0963670c6de8767afb6c67d88d856b0c0dad.png'
+
+            e = discord.Embed(color=0xFFA500)
+            e.set_author(name='\u200b', icon_url=urban_icon)
             if len(definition) > 1024:
                 e.add_field(name=search_term, value=f'{definition[:1021].replace("[", "").replace("]","")}...')
             elif len(definition) < 1024:
                 e.add_field(name=search_term, value=definition.replace("[", "").replace("]",""))
-            e.set_thumbnail(url='https://d2gatte9o95jao.cloudfront.net/assets/apple-touch-icon-1734beeaa059fbc5587bddb3001a0963670c6de8767afb6c67d88d856b0c0dad.png')
             e.add_field(name='\u200b', value=self.urban_url + search_term_url, inline=False)
 
             await ctx.send(embed=e)
-            logging.info(f'[Dictionary Returned]: Embed response for {self.api_url + search_term_url}')
+            logging.info(f'[Urban Dictionary Returned]: Embed response for {self.api_url + search_term_url}')
         except IndexError:
-            await ctx.send(f'No results for {search_term}')
-            logging.info(f'[Dictionary Error]: No results for {search_term}')
+            if slash_check:
+                await ctx.send(f'No results for "{search_term}"', hidden=True)
+            else:
+                await ctx.send(f'No results for "{search_term}"')
+            logging.info(f'[Urban Dictionary Error]: No results for {search_term}')
 
     @cog_ext.cog_slash(name="dict", description="Definitions from Merriam-Webster", guild_ids=guild_ids)
     async def slash_dict(self, ctx: SlashContext, search_term):
@@ -53,7 +57,7 @@ class Dictionary(commands.Cog):
 
     @cog_ext.cog_slash(name="urban", description="Definitions from Urban Dictionary", guild_ids=guild_ids)
     async def slash_urban(self, ctx: SlashContext, search_term):
-        await self.urban(ctx, search_term)
+        await self.urban(ctx, search_term, slash_check=True)
 
     @commands.command(name="urban", pass_context=True, brief="Definitions from Urban Dictionary",
     description="Use quotes when searching for a phrase using the prefix command - Example: !urban \"search phrase\"")
